32c56eb8832eb0710c4ed10c1059639a9bccfba2
Ayekaba <ayekaba.mengue@hotmail.com>
2020-07-31 21:58:20 +0200

    Commit 2

--- app.js
+++ app.js
@@ -65,6 +65,7 @@ class UI
         });
         productsDOM.innerHTML = result;
     }
+
     getBagButtons()
     {
         // array of buttons
@@ -92,10 +93,75 @@ class UI
                             // set cart values
                             this.setCartValues(cart);
                             // display cart item
+                            this.addCartItem(cartItem);
                             // show cart
+                            this.showCart();
                         });
                 });
     }
+
+    setCartValues(cart) 
+    {
+        let tempTotal  = 0;
+        let itemsTotal = 0;
+        cart.map(item => 
+        {
+            tempTotal += item.price * item.amount;
+            itemsTotal += item.amount;
+        });
+        cartTotal.innerText = parseFloat(tempTotal.toFixed(2));
+        cartItems.innerText = itemsTotal;
+    }
+
+    addCartItem(item)
+    {
+        const div = document.createElement('div');
+            div.classList.add('cart-item');
+            div.innerHTML = `
+            <img src="${item.image}" alt="product">
+            <div>
+                <h4>${item.title}</h4>
+                <h5>${item.price}€</h5>
+                <span class="remove-item" data-id=${item.id}>remove</span>
+            </div>
+            <div>
+                <i class="fas fa-chevron-up" data-id=${item.id}></i>
+                <p class="item-amount">${item.amount}</p>
+                <i class="fas fa-chevron-down" data-id=${item.id}></i>
+
+            </div>
+            `;
+        cartContent.appendChild(div);
+    }
+
+    showCart()
+    {
+        cartOvelay.classList.add('transparentBcg');
+        cartDOM.classList.add('showCart');
+    }
+
+   
+    setupAPP()
+    {
+        cart = Storage.getCart();
+        this.setCartValues(cart);
+        this.populateCart(cart);
+        cartBtn.addEventListener('click', this.showCart);
+        closeCartBtn.addEventListener('click', this.hideCart);
+    }
+
+    populateCart(cart)
+    {
+        cart.forEach(item => this.addCartItem(item));
+    }
+
+    hideCart()
+    {
+        cartOvelay.classList.remove('transparentBcg');
+        cartDOM.classList.remove('showCart');
+    }
+
+    
 }
 
 // local storage
@@ -116,6 +182,11 @@ class Storage
     {
         localStorage.setItem('cart', JSON.stringify(cart));
     } 
+
+    static getCart()
+    {
+        return localStorage.getItem('cart') ? JSON.parse(localStorage.getItem('cart')): [];
+    }
 }
 
 
@@ -123,7 +194,8 @@ class Storage
 document.addEventListener("DOMContentLoaded", ()=>{
     const ui = new UI();
     const products = new Products();
-
+    // setup app
+    ui.setupAPP();
 
     // get all products
     products.getProducts().then(products => 
